d141f84e2467499bbdaa321c261b79fd23c471a7
bobrobcpp <king.roob@gmail.com>
2018-01-12 22:20:35 +0000

    Add projects view and update server.js accordingly

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    projectsMessage: 'This is the projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'You have done wrong by me'
